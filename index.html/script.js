a9b2900e4ec705dae143400ca902475570c4f5a8
MateoAlzate <brolkwin@gmail.com>
2026-03-25 20:29:29 -0500

    de celcius a farenheith

--- index.html/script.js
+++ index.html/script.js
@@ -1,7 +1,7 @@
 
-let dolar = Number(prompt("¿Cuantos pesos quieres convertir a dolares?"));
-let cop = dolar / 0.00027
+let celcius = Number(prompt("¿Cuantos grados celcius quieres convertir?"));
+let fahrenheit = (celcius * 1.8) + 32;
 
-console.log("En total son $",cop," pesos")
+console.log("En total son ",fahrenheit," fahrenheit")
 
 
