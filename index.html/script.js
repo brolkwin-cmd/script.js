d972a388252d17bb1c6f5f0214ca5da4b7b60185
MateoAlzate <brolkwin@gmail.com>
2026-03-26 17:51:38 -0500

    dolares a pesos 2

--- index.html/script.js
+++ index.html/script.js
@@ -2,7 +2,4 @@
 let centimetro  = Number(prompt("¿Cuantos centimetros quieres convertir?"));
 let milimetro = centimetro * 10
 
-console.log("El  total de milimetros es: ",milimetro)
-
-
-
+console.log("El  total de milimetros es: ",milimetro);
