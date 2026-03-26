be6c9fdd3a460ae38f34e42e99edd27dc54cbef0
MateoAlzate <brolkwin@gmail.com>
2026-03-25 20:44:55 -0500

    centimetros a milimetros

--- index.html/script.js
+++ index.html/script.js
@@ -1,8 +1,8 @@
 
-let milimetro  = Number(prompt("¿Cuantos milimetros quieres convertir?"));
-let centimetro = milimetro / 10
+let centimetro  = Number(prompt("¿Cuantos centimetros quieres convertir?"));
+let milimetro = centimetro * 10
 
-console.log("El  total de centimetros es: ",centimetro)
+console.log("El  total de milimetros es: ",milimetro)
 
 
 
