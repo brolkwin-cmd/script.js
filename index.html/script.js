54a49582c23b6d12219d5bdc788e868f3df67153
MateoAlzate <brolkwin@gmail.com>
2026-03-26 20:18:40 -0500

    jercicio de cona

--- index.html/script.js
+++ index.html/script.js
@@ -1,5 +1,20 @@
+let costoCena = Number(prompt(`¿Cuanto costo la cena?`));
+let comensales = Number(prompt(`¿Cuantas comenzales hubo en la cena?`));
+let gloton = (prompt(`¿Quien fue la persona que más comío?`));
+let anemico = (prompt(`¿Quien fue la persona que comio menos?`))
+let totalfraccion = 2 + 0.5 + (comensales - 2 )
+let valorfraccion = costoCena / totalfraccion
+let valorgloton = valorfraccion * 2
+let valoranemico = valorfraccion * 0.5  
+let preciofinal = valorfraccion
 
-let centimetro  = Number(prompt("¿Cuantos centimetros quieres convertir?"));
-let milimetro = centimetro * 10
 
-console.log("El  total de milimetros es: ",milimetro);
+
+
+
+console.log(`Gracias por comer con nosotros: en total son: $${costoCena}, y fueron ${comensales} comensales, pero ${gloton}, fue el que más consumio, por lo tanto pagará: ${valorgloton.toFixed(2)}, y ${anemico} fue el que menos comio, por lo que debera pagar: $ ${valoranemico.toFixed(2)}, por lo tanto, todos los demás deben cancelar: ${valorfraccion.toFixed(2)}`); 
+
+
+
+
+s
