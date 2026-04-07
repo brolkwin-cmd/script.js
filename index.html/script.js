be00af6bc36cc85c7b6626603c0bebdc93aedb16
MateoAlzate <brolkwin@gmail.com>
2026-04-06 19:45:58 -0500

    Promedio Notas

--- index.html/script.js
+++ index.html/script.js
@@ -1,9 +1,11 @@
-let productos =  []
+let promedioNotas =  []
+let nota1 = Number(prompt("Ingrese la primera nota a promediar"))
+let nota2 = Number(prompt("Ingrese la SEgunda nota a promediar"))
+let nota3 = Number(prompt("Ingrese la Tercera nota a promediar"))
+let nota4 = Number(prompt("Ingrese la Cuarta nota a promediar"))
+let nota5 = Number(prompt("Ingrese la Quintaa nota a promediar"))
 
-let product1 = prompt("Ingrese el primer producto que desea comprar")
-let product2 = prompt("Ingrese el segundo producto")
-let product3 = prompt("Ingrese el último producto")
-
-productos.push(product1,product2,product3)
-console.log(productos)
+promedioNotas.push(nota1,nota2,nota3,nota4,nota5)
+let promedio = (nota1+nota2+nota3+nota4+nota5)/(promedioNotas.length)
+console.log(promedio)
 
