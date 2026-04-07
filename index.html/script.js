7872aa645cdaa73e66aa8b644c27ae78cebee556
MateoAlzate <brolkwin@gmail.com>
2026-04-06 20:44:48 -0500

    Precio Descuento

--- index.html/script.js
+++ index.html/script.js
@@ -1,11 +1,22 @@
-let promedioNotas =  []
+/*let promedioNotas =  []
 let nota1 = Number(prompt("Ingrese la primera nota a promediar"))
 let nota2 = Number(prompt("Ingrese la SEgunda nota a promediar"))
 let nota3 = Number(prompt("Ingrese la Tercera nota a promediar"))
 let nota4 = Number(prompt("Ingrese la Cuarta nota a promediar"))
 let nota5 = Number(prompt("Ingrese la Quintaa nota a promediar"))
-
 promedioNotas.push(nota1,nota2,nota3,nota4,nota5)
 let promedio = (nota1+nota2+nota3+nota4+nota5)/(promedioNotas.length)
-console.log(promedio)
+console.log(promedio)*/
 
+
+let precioProducto = Number(prompt("Ingrese el valor del producto"))
+
+if(precioProducto <= 100 )
+    { console.log(precioProducto * 1.1)
+}
+else if (precioProducto>100 && precioProducto<=200)
+    { console.log(precioProducto * 0.9)}
+
+else console.log (precioProducto * 0.75)
+
+ 
